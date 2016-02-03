37b898c69818f5ace06ea8789721d3af6318acd7
LauvChris <christophe.lauv@hotmail.fr>
2016-02-03 10:34:15 +0100

    petites modifs activities

--- app/src/main/java/miage/uparis10/fr/mabu/InfosActivity.java
+++ app/src/main/java/miage/uparis10/fr/mabu/InfosActivity.java
@@ -7,14 +7,14 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class InfosActivity extends AppCompatActivity {
-        String test = "200 Avenue de la République, 92000 Nanterre";
+        String TexteAdresse = "200 Avenue de la République, 92000 Nanterre";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infos);
         TextView adresse = (TextView) findViewById(R.id.textViewAdresse);
-        adresse.setText(test);
+        adresse.setText(TexteAdresse);
 
         TextView horaires = (TextView) findViewById(R.id.textViewHoraires);
         horaires.setText("Lundi 9h30 - 19h\nMardi 9h30 - 19h\nMercredi 9h30 - 19h\nJeudi 9h30 - 19h\nVendredi 9h30 - 19h\nSamedi 9h30 - 19h\n ");
