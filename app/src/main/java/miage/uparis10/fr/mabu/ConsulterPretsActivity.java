528b2dab403182954a92a8046fe56d35c2658645
ClementLerouge <clementlerouge@free.fr>
2016-02-04 14:37:35 +0100

    Afficher les livre emprunté
    Affichage les livre pris.
    Logo + Titre + petit resumé.

--- app/src/main/java/miage/uparis10/fr/mabu/ConsulterPretsActivity.java
+++ app/src/main/java/miage/uparis10/fr/mabu/ConsulterPretsActivity.java
@@ -1,12 +1,22 @@
 package miage.uparis10.fr.mabu;
 
-import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class ConsulterPretsActivity extends AppCompatActivity {
-    ListView maListe;
+    ListView maListViewPerso;
     String[] prenoms = new String[]{
             "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
             "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
@@ -19,10 +29,73 @@ public class ConsulterPretsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulter_prets);
-        maListe = (ListView) findViewById(R.id.listView2);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ConsulterPretsActivity.this,
-                android.R.layout.simple_list_item_1, prenoms);
-        maListe.setAdapter(adapter);
+//Récupération de la listview créée dans le fichier main.xml
+        maListViewPerso = (ListView) findViewById(R.id.listView);
+
+        //Création de la ArrayList qui nous permettra de remplire la listView
+        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
+
+        //On déclare la HashMap qui contiendra les informations pour un item
+        HashMap<String, String> map;
+
+        //Création d'une HashMap pour insérer les informations du premier item de notre listView
+        map = new HashMap<String, String>();
+        //on insère un élément titre que l'on récupérera dans le textView titre créé dans le fichier affichageitem.xml
+        map.put("titre", "Comment devenir Dieu en 666 étapes");
+        //on insère un élément description que l'on récupérera dans le textView description créé dans le fichier affichageitem.xml
+        map.put("description", "Il faut le lire pour le savoir :p");
+        //on insère la référence à l'image (convertit en String car normalement c'est un int) que l'on récupérera dans l'imageView créé dans le fichier affichageitem.xml
+        map.put("img", String.valueOf(R.drawable.icone_book));
+        //enfin on ajoute cette hashMap dans la arrayList
+        listItem.add(map);
+
+        //On refait la manip plusieurs fois avec des données différentes pour former les items de notre ListView
+
+        map = new HashMap<String, String>();
+        map.put("titre", "RTFM");
+        map.put("description", "Lit le putain de manuel !!");
+        map.put("img", String.valueOf(R.drawable.icone_book));
+        listItem.add(map);
+
+        map = new HashMap<String, String>();
+        map.put("titre", "Savoir coder pour les Noob");
+        map.put("description", "Un livre pour les noob que les pro n'utilise pas ");
+        map.put("img", String.valueOf(R.drawable.icone_book));
+        listItem.add(map);
+
+        map = new HashMap<String, String>();
+        map.put("titre", "Pouic");
+        map.put("description", "le livre des poule");
+        map.put("img", String.valueOf(R.drawable.icone_book));
+        listItem.add(map);
+
+        //Création d'un SimpleAdapter qui se chargera de mettre les items présent dans notre list (listItem) dans la vue affichageitem
+        SimpleAdapter mSchedule = new SimpleAdapter (this.getBaseContext(), listItem, R.layout.item_liste_prets,
+                new String[] {"img", "titre", "description"}, new int[] {R.id.img, R.id.titre, R.id.description});
+
+        //On attribut à notre listView l'adapter que l'on vient de créer
+        maListViewPerso.setAdapter(mSchedule);
+
+        //Enfin on met un écouteur d'évènement sur notre listView
+        maListViewPerso.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            @SuppressWarnings("unchecked")
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                //on récupère la HashMap contenant les infos de notre item (titre, description, img)
+                HashMap<String, String> map = (HashMap<String, String>) maListViewPerso.getItemAtPosition(position);
+                //on créer une boite de dialogue
+                AlertDialog.Builder adb = new AlertDialog.Builder(ConsulterPretsActivity.this);
+                //on attribut un titre à notre boite de dialogue
+                adb.setTitle("Sélection Item");
+                //on insère un message à notre boite de dialogue, et ici on affiche le titre de l'item cliqué
+                adb.setMessage("Votre choix : "+map.get("titre"));
+                //on indique que l'on veut le bouton ok à notre boite de dialogue
+                adb.setPositiveButton("Ok", null);
+                //on affiche la boite de dialogue
+                adb.show();
+            }
+        });
+
     }
 }
