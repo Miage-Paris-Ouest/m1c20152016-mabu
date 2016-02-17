018ae82f22670447e056e759b00fa56ff93c1d5d
LauvChris <christophe.lauv@hotmail.fr>
2016-02-17 22:07:54 +0100

    Début Prolongation + améliorations emprunt/actus

--- app/src/main/java/miage/uparis10/fr/mabu/ConsulterPretsActivity.java
+++ app/src/main/java/miage/uparis10/fr/mabu/ConsulterPretsActivity.java
@@ -11,6 +11,8 @@ import java.util.HashMap;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -61,9 +63,9 @@ public class ConsulterPretsActivity extends AppCompatActivity {
                 }
 
                 map = new HashMap<String, String>();
-                map.put("titre", "Titre : "+liste.get(j));
+                map.put("titre", liste.get(j));
                 j++;
-                map.put("description","Resumé : "+liste.get(j));
+                map.put("date","A rendre avant : "+liste.get(j));
                 j++;
                 map.put("img", String.valueOf(R.drawable.icone_book));
                 listItem.add(map);
@@ -77,7 +79,7 @@ public class ConsulterPretsActivity extends AppCompatActivity {
 
         //Création d'un SimpleAdapter qui se chargera de mettre les items présent dans notre list (listItem) dans la vue affichageitem
         SimpleAdapter mSchedule = new SimpleAdapter (this.getBaseContext(), listItem, R.layout.item_liste_prets,
-                new String[] {"img", "titre", "description"}, new int[] {R.id.img, R.id.titre, R.id.description});
+                new String[] {"img", "titre", "date"}, new int[] {R.id.img, R.id.titre, R.id.date});
 
         //On attribut à notre listView l'adapter que l'on vient de créer
         maListViewPerso.setAdapter(mSchedule);
@@ -88,19 +90,26 @@ public class ConsulterPretsActivity extends AppCompatActivity {
             @SuppressWarnings("unchecked")
             public void onItemClick(AdapterView<?> a, View v, int position, long id) {
                 //on récupère la HashMap contenant les infos de notre item (titre, description, img)
-                HashMap<String, String> map = (HashMap<String, String>) maListViewPerso.getItemAtPosition(position);
+                final HashMap<String, String> map = (HashMap<String, String>) maListViewPerso.getItemAtPosition(position);
                 //on créer une boite de dialogue
                 AlertDialog.Builder adb = new AlertDialog.Builder(ConsulterPretsActivity.this);
                 //on attribut un titre à notre boite de dialogue
-                adb.setTitle("Sélection Item");
+                adb.setTitle(map.get("titre"));
                 //on insère un message à notre boite de dialogue, et ici on affiche le titre de l'item cliqué
-                adb.setMessage("Votre choix : "+map.get("titre"));
-                //on indique que l'on veut le bouton ok à notre boite de dialogue
-                adb.setPositiveButton("Ok", null);
-                //on affiche la boite de dialogue
-                adb.show();
-            }
-        });
+                adb.setMessage("Souhaitez-vous prolonger ?");
+                //boutons oui et non boite de dialoguqe
+                adb.setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intentProlonger = new Intent(getApplicationContext(), ProlongationActivity.class);
+                        intentProlonger.putExtra("titre",map.get("titre"));
+                        startActivity(intentProlonger);
+                    }
+                });
+                    adb.setNegativeButton("Non", null);
+                    //on affiche la boite de dialogue
+                    adb.show();
+                }
+            });
 
     }
 }
--- app/src/main/java/miage/uparis10/fr/mabu/ProlongationActivity.java
+++ app/src/main/java/miage/uparis10/fr/mabu/ProlongationActivity.java
@@ -1,7 +1,9 @@
 package miage.uparis10.fr.mabu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProlongationActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ProlongationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prolongation);
+        Intent intent = getIntent();
+        String titre = intent.getStringExtra("titre");
+
+        TextView textViewLogin = (TextView) findViewById(R.id.textViewTitre);
+        textViewLogin.setText("Vous allez prolonger : " + titre);
+
     }
 }
