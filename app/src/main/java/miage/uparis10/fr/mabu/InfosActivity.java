a570ab900d4a72234985943d131d68714ae62514
ClementLerouge <clementlerouge@free.fr>
2016-02-24 14:54:16 +0100

    Lecture Internet
    ajout des sortie internet
    3 test de recupération du code Html

--- app/src/main/java/miage/uparis10/fr/mabu/InfosActivity.java
+++ app/src/main/java/miage/uparis10/fr/mabu/InfosActivity.java
@@ -8,9 +8,15 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -64,6 +70,83 @@ public class InfosActivity extends AppCompatActivity {
         catch (Exception e){
             System.out.println(e.toString());
         }
+
+        // Recuperer contenu de la vrais page
+
+//TEST 1
+/*
+        HttpURLConnection conn = null;
+        try {
+            conn = (HttpURLConnection) new URL("http://scd.u-paris10.fr/").openConnection();
+            conn.connect();
+
+            BufferedInputStream bis = new BufferedInputStream(conn.getInputStream());
+
+            byte[] bytes = new byte[1024];
+            int tmp ;
+            while( (tmp = bis.read(bytes) ) != -1 ) {
+                String chaine = new String(bytes,0,tmp);
+                System.out.print(chaine);
+            }
+
+            conn.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+*/
+
+
+//Test 2
+  /*          String toreturn = null;
+            try {
+                //Creation d'un objet URL
+                URL url = new URL("http://scd.u-paris10.fr/");
+                //on etablie une connection a cette url
+                URLConnection uc = url.openConnection();
+                //on y cree un flux de lecture
+                InputStream in = uc.getInputStream();
+                //on lit le premier bit
+                int c = in.read();
+                //on cree un StringBuilder pour par la suite y ajouter tout les bit lus
+                StringBuilder build = new StringBuilder();
+                //tant que c n'est pas egale au bit indiquant la fin d'un flux...
+                while (c != -1) {
+                    build.append((char) c);
+                //...on l'ajoute dasn le StringBuilder...
+                    c = in.read();
+                //...on lit le suivant
+                }
+                //on retourne le code de la page
+                toreturn = build.toString();
+
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+            System.out.print(toreturn);
+
+*/
+//TEST 3
+/*
+        URL oracle = null;
+        try {
+            oracle = new URL("http://www.oracle.com/");
+            URLConnection yc = oracle.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    yc.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                System.out.println(inputLine);
+            in.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
 
         //Création d'un SimpleAdapter qui se chargera de mettre les items présent dans notre list (listItem) dans la vue affichageitem
         SimpleAdapter mSchedule = new SimpleAdapter (this.getBaseContext(), listItem, R.layout.item_liste_actus,
