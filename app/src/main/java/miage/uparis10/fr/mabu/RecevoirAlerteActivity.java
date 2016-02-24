5c882edb6b6c6423ea629a5f6e9af78087943152
Hemeris <menguy.philippe@laposte.net>
2016-02-24 12:59:53 +0100

    Maj - Notification
    Ajout d'une configuration par l'utilisateur pour mettre une alerte.

--- app/src/main/java/miage/uparis10/fr/mabu/RecevoirAlerteActivity.java
+++ app/src/main/java/miage/uparis10/fr/mabu/RecevoirAlerteActivity.java
@@ -2,8 +2,7 @@ package miage.uparis10.fr.mabu;
 
 
 import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.NotificationManager;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -11,17 +10,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 import java.util.Calendar;
+
+import static java.lang.Integer.parseInt;
 
 public class RecevoirAlerteActivity extends AppCompatActivity {
 
     private Button notification5m;
     private Button notification1h;
-    private Button notification1j;
-    private Button notification7j;
     private Button notification5m_avant;
+    private Button notification_prets;
+    private EditText choixJour;
     AlarmManager alarmManager;
 
     @Override
@@ -31,54 +33,51 @@ public class RecevoirAlerteActivity extends AppCompatActivity {
 
         notification5m = (Button) findViewById(R.id.notification5m);
         notification1h = (Button) findViewById(R.id.notification1h);
-        notification1j = (Button) findViewById(R.id.notification1j);
-        notification7j = (Button) findViewById(R.id.notification7j);
         notification5m_avant = (Button) findViewById(R.id.notification5m_avant);
-
+        notification_prets = (Button) findViewById(R.id.notification_prets);
 
         alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         notification5m.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                ajouterAlarme(5,1,1);
+                ajouterAlarme(5, 1, 1);
             }
         });
         notification1h.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                ajouterAlarme(60,1,1);
+                ajouterAlarme(60, 1, 1);
             }
         });
-        notification1j.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                ajouterAlarme(60,24,1);
-            }
-        });
-        notification7j.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                ajouterAlarme(60,24,7);
-            }
-        });
-
         notification5m_avant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
                 Calendar calendar = Calendar.getInstance();
                 int dateCourante = calendar.get(Calendar.DAY_OF_MONTH);
-                int dateRendu = 18;
+                int dateRendu = calendar.get(Calendar.DAY_OF_MONTH) + 1;
                 int dateRestant = dateRendu - dateCourante;
-                ajouterAlarme(5,1,dateRestant);
+                ajouterAlarme(1, 1, dateRestant);
+            }
+        });
+        notification_prets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                choixJour = (EditText) findViewById(R.id.choixJour);
+                String recup = choixJour.getText().toString();
+                int jour =  parseInt(recup);
+                ajouterAlarme(1, 1, jour);
+                AlertDialog.Builder adb = new AlertDialog.Builder(RecevoirAlerteActivity.this);
+                adb.setTitle("Info");
+                adb.setMessage("Notification créée avec succès");
+                adb.setPositiveButton("Ok", null);
+                adb.show();
             }
         });
     }
-
-    public void ajouterAlarme(int minutes, int heures, int jours) {
-        /*Calendar calendar = Calendar.getInstance();
-        calendar.set(2016,02,11,12,43);*/
+    public void ajouterAlarme(int minutes, int heures, int jours){
         Intent intent = new Intent(RecevoirAlerteActivity.this, TimeAlertes.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 1, intent, PendingIntent.FLAG_ONE_SHOT);
-        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+(1000*60*minutes*heures*jours), pendingIntent);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (1000 * 60 * minutes * heures * jours), pendingIntent);
     }
+
 }
